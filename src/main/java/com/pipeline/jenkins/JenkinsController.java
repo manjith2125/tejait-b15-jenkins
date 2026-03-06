beb7bc4d82389b170d57398e6db1128b7e330740
manjth2125 <manjith2125@gmail>
2026-03-06 17:54:38 +0530

    added new api

--- src/main/java/com/pipeline/jenkins/JenkinsController.java
+++ src/main/java/com/pipeline/jenkins/JenkinsController.java
@@ -13,4 +13,10 @@ public class JenkinsController {
         return "Welcome to Jenkins user Manjith";
     }
     // added comment for maven to test weather it is updating in every minute in Jenkins
+
+@GetMapping("hello")
+    public String hello(){
+
+        return  "Hello manjith.i am jenkins how can i help you";
+    }
 }
